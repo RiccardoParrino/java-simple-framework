7afdb001e7d98a9e08a107c5e106d01a5911ae41
RiccardoParrino <riccardoparrino1@gmail.com>
2025-11-05 08:47:05 +0100

    add simple example

---
+++ test-app/src/main/java/com/parrino/riccardo/repository/MyRepository.java
@@ -0,0 +1,8 @@
+package com.parrino.riccardo.repository;
+
+import org.simple.framework.beans.annotations.Bean;
+
+@Bean
+public class MyRepository {
+    
+}
---
+++ test-app/src/main/java/com/parrino/riccardo/service/MyService.java
@@ -0,0 +1,8 @@
+package com.parrino.riccardo.service;
+
+import org.simple.framework.beans.annotations.Bean;
+
+@Bean
+public class MyService {
+    
+}
